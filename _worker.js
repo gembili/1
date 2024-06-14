bb2096a9a4727fa8ce74facbd87ce5f5897c9bd3
gembili <136712442+gembili@users.noreply.github.com>
2024-06-15 04:11:47 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '34a3f3a6-7e5e-4db5-8a14-665057710db7';
 
-const proxyIPs = ['194.36.179.45'];
+const proxyIPs = ['103.167.151.182'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
